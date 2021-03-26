53c30d5b5e365c54c8de5d133ca4c12831b14ac7
sidahmed <ahorob57@gmail.com>
2021-03-26 20:47:44 +0100

    change the about me text

--- src/components/Common/About/About.jsx
+++ src/components/Common/About/About.jsx
@@ -31,14 +31,12 @@ export default function About({ theme }) {
         <div className="about-content">
           <h3>About Me</h3>
           <p>
-            I am high school dropout with retail and customer service jobs, and i started
-            programming and web developement in april 2019 till now. so i consider my self a
-            self-taught Software Engineer with a autodidactic mindset seeking to develop a 
+            Hi there, my name is Ahmed from Algeria
+            a self-taught Software Engineer with a autodidactic mindset seeking to develop a 
             flexible, robust and easy to scale software and distributed systems. 
             and I have been able to deliver a consistent approach throughout a period of 
             challenges so far with committment to learning and self-development.
           </p>
-          
           <br/>
           <br/>
 
